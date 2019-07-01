7cf007060a4545c947803ed9647ec084f675d555
TheRitual <dev@theritual.eu>
2019-07-01 22:52:50 +0200

    Created Calculator class and Main operations

--- src/main/java/com/kodilla/calculator/CalculatorApplication.java
+++ src/main/java/com/kodilla/calculator/CalculatorApplication.java
@@ -8,6 +8,9 @@ public class CalculatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApplication.class, args);
+        System.out.println(Calculator.add(1,2));
+        System.out.println(Calculator.substract(5,7));
+        System.exit(0);
     }
 
 }
